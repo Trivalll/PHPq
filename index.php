6bdd207e85ebe83644723154a01a7d306da2a6b8
Trivalll <96657104+Trivalll@users.noreply.github.com>
2022-04-02 09:26:00 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title>1 тапсырма</title>
+  </head>
+  <body>
+      <?php
+      $a = 45;
+      $b = 25;
+      $d = 7;
+      $c = $a - $b;
+      $result = $c + $d;
+      echo($result); ?>
+      
+      
+  </body>
+</html>
